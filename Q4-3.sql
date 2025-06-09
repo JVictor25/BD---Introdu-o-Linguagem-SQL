401b2eca50cfddeda8fee4b5dcbb71720f71b81d
JVictor <joaovictorguedes2003@hotmail.com>
2025-06-09 15:42:53 -0300

    Q4-3

--- Q4-3.sql
+++ Q4-3.sql
@@ -2,5 +2,5 @@ select parts.pname
 from parts
 join sppj on parts.pno=sppj.parts_pno
 group by parts.pno
-having count(distinct sppj.suppliers_sno)=(
-select count(*) from suppliers);
+having count(distinct sppj.suppliers_sno)=
+(select count(*) from suppliers); 
